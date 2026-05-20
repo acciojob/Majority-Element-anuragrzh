2e413e116b0981642df2d854229c375895a57562
anuragsoni5643 <sonianurag506@gmail.com>
2026-05-20 18:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+
+function majElement(){
+	                                                                  
+	let arr=n.split(" ")
+	let value=Math.floor(arr.length/2)
+	let narr={}
+	console.log(arr.length)
+	  for (let i = 0; i < arr.length; i++) {
+
+        let el = arr[i];
+
+        narr[el] = (narr[el] || 0) + 1;
+
+        if (narr[el] > value) {
+            console.log(el);
+            return;
+        }
+}
+	console.log("No Majority Element");
+}
+let n= prompt()    
+majElement(n)
